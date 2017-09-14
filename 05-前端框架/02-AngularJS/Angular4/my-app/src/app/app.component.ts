0dfc0f967c7a65e65f7c7ce1a36e1503d1a8a1e8
csxiaoyao <1724338257@qq.com>
2017-09-14 16:37:42 +0800

    angular

---
+++ 05-前端框架/02-AngularJS/Angular4/my-app/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+	  <h1>{{title}}</h1>
+	  <h2>{{hero.name}} details!</h2>
+	  <div><label>id: </label>{{hero.id}}</div>
+	  <div><label>name: </label>{{hero.name}}</div>
+
+	  <div>
+		  <label>name: </label>
+		  <input [(ngModel)]="hero.name" placeholder="name">
+		</div>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	title = 'Tour of Heroes';
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
+}
---
+++ 05-前端框架/02-AngularJS/Angular4/my-app/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
