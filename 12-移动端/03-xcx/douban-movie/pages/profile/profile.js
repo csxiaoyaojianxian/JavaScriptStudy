d19ca3d4ede72793390c84633c73be9744f0afec
csxiaoyao <1724338257@qq.com>
2018-03-22 10:11:36 +0800

    xcx

---
+++ 12-移动端/03-xcx/douban-movie/pages/profile/profile.js
@@ -0,0 +1,34 @@
+Page({
+  data: {
+    title: 'About Me',
+    userInfo: {
+      wechat: 'WEDN-NET',
+      nickName: 'iceStone',
+      avatarUrl: '../../images/wechat.jpeg'
+    }
+  },
+
+  getUserInfo () {
+    const that = this
+    wx.getUserInfo({
+      success (res) {
+        console.log(res)
+        that.setData({ userInfo: res.userInfo })
+      }
+    })
+  },
+
+  onLoad () {
+    wx.login({
+      success (res) {
+        if (res.code) {
+          console.log('登录成功！' + res.code)
+        } else {
+          console.error('获取用户登录态失败！' + res.errMsg)
+        }
+      },
+      fail () {},
+      complete () {},
+    })
+  }
+})
